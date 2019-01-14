cbb5f4b6392916fa711bac1b2d19db49b10bc8a8
Richard Collier <ergosoftuk@gmail.com>
2019-01-14 17:13:43 +0000

    Try puppeteer

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -27,28 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['Chrome_without_security'],
-    customLaunchers:{
-      Chrome_without_security:{
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--enable-logging=stderr',
-          '--no-default-browser-check',
-          '--no-first-run',
-          '--disable-default-apps',
-          '--disable-popup-blocking',
-          '--disable-translate',
-          '--disable-background-timer-throttling',
-          '--disable-renderer-backgrounding',
-          '--disable-device-discovery-notifications',
-          '--remote-debugging-port=9222',
-          '--disable-web-security',
-          '--v=1'
-        ]
-      }
-    },
+    browsers: ['ChromeHeadless'],
     singleRun: true
   });
 };
