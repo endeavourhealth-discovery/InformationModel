d1730aa901b5f982f9830c79d00ff01f38e9b25a
Richard Collier <ergosoftuk@gmail.com>
2019-01-22 11:57:09 +0000

    Try puppeteer

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,10 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
-const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision;
-const Downloader = require('puppeteer/utils/ChromiumDownloader');
-const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision);
-
-process.env.CHROME_BIN = revisionInfo.executablePath;
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -35,10 +31,13 @@ module.exports = function (config) {
     browsers: ['HeadlessChrome'],
     customLaunchers:{
       HeadlessChrome:{
-        base: 'ChromeHeadless',
+        base: 'ChromiumHeadless',
         flags: [
           '--no-sandbox',
+          '--headless',
           '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
           // Without a remote debugging port, Google Chrome exits immediately.
           '--remote-debugging-port=9222',
           '--disable-web-security'
