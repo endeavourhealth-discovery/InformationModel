1707ae050d55dc17c0d6d6157b1561dab2ff9322
Richard Collier <ergosoftuk@gmail.com>
2019-07-15 14:25:10 +0100

    Add metrics to client

--- client/src/main/java/org/endeavourhealth/im/client/IMClient.java
+++ client/src/main/java/org/endeavourhealth/im/client/IMClient.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.endeavourhealth.common.cache.ObjectMapperPool;
 import org.endeavourhealth.common.config.ConfigManager;
 import org.endeavourhealth.common.security.keycloak.client.KeycloakClient;
+import org.endeavourhealth.common.utility.MetricsHelper;
+import org.endeavourhealth.common.utility.MetricsTimer;
 
 
 import javax.ws.rs.client.Client;
@@ -16,7 +18,8 @@ import java.util.Map;
 
 public class IMClient {
     private static final String base = "/client";
-    private static JsonNode config;
+    private static JsonNode kcConfig;
+    private static String imUrl;
     private static KeycloakClient kcClient;
 
     public static Integer getConceptIdForSchemeCode(String scheme, String code) throws Exception {
@@ -24,43 +27,49 @@ public class IMClient {
     }
 
     public static Integer getConceptIdForSchemeCode(String scheme, String code, Boolean autoCreate, String term) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("scheme", scheme);
-        params.put("code", code);
-        params.put("autoCreate", autoCreate.toString());
-        params.put("term", term);
+        try (MetricsTimer timer =MetricsHelper.recordTime("getConceptIdForSchemeCode")) {
+            Map<String, String> params = new HashMap<>();
+            params.put("scheme", scheme);
+            params.put("code", code);
+            params.put("autoCreate", autoCreate.toString());
+            params.put("term", term);
 
-        Response response = get(base + "/Concept", params);
+            Response response = get(base + "/Concept", params);
 
-        if (response.getStatus() == 200)
-            return response.readEntity(Integer.class);
-        else
-            throw new IOException(response.readEntity(String.class));
+            if (response.getStatus() == 200)
+                return response.readEntity(Integer.class);
+            else
+                throw new IOException(response.readEntity(String.class));
+        }
     }
 
     public static Integer getMappedCoreConceptIdForSchemeCode(String scheme, String code) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("scheme", scheme);
-        params.put("code", code);
+        try (MetricsTimer timer =MetricsHelper.recordTime("getMappedCoreConceptIdForSchemeCode")) {
+            Map<String, String> params = new HashMap<>();
+            params.put("scheme", scheme);
+            params.put("code", code);
 
-        Response response = get(base + "/Concept/Core", params);
+            Response response = get(base + "/Concept/Core", params);
 
-        if (response.getStatus() == 200)
-            return response.readEntity(Integer.class);
-        else
-            throw new IOException(response.readEntity(String.class));
+            if (response.getStatus() == 200)
+                return response.readEntity(Integer.class);
+            else
+                throw new IOException(response.readEntity(String.class));
+        }
     }
 
     public static String getCodeForConceptId(Integer conceptId) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("dbid", conceptId.toString());
+        try (MetricsTimer timer =MetricsHelper.recordTime("getCodeForConceptId")) {
+            Map<String, String> params = new HashMap<>();
+            params.put("dbid", conceptId.toString());
 
-        Response response = get(base + "/Concept/Code", params);
+            Response response = get(base + "/Concept/Code", params);
 
-        if (response.getStatus() == 200)
-            return response.readEntity(String.class);
-        else
-            throw new IOException(response.readEntity(String.class));
+            if (response.getStatus() == 200)
+                return response.readEntity(String.class);
+            else
+                throw new IOException(response.readEntity(String.class));
+        }
     }
 
     public static Integer getConceptIdForTypeTerm(String type, String term) throws Exception {
@@ -68,48 +77,60 @@ public class IMClient {
     }
 
     public static Integer getConceptIdForTypeTerm(String type, String term, Boolean autoCreate) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("type", type);
-        params.put("term", term);
-        params.put("autoCreate", autoCreate.toString());
+        try (MetricsTimer timer =MetricsHelper.recordTime("getConceptIdForTypeTerm")) {
+            Map<String, String> params = new HashMap<>();
+            params.put("type", type);
+            params.put("term", term);
+            params.put("autoCreate", autoCreate.toString());
 
-        Response response = get(base + "/Term", params);
+            Response response = get(base + "/Term", params);
 
-        if (response.getStatus() == 200)
-            return response.readEntity(Integer.class);
-        else
-            throw new IOException(response.readEntity(String.class));
-
+            if (response.getStatus() == 200)
+                return response.readEntity(Integer.class);
+            else
+                throw new IOException(response.readEntity(String.class));
+        }
     }
 
     public static Integer getMappedCoreConceptIdForTypeTerm(String type, String term) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        params.put("type", type);
-        params.put("term", term);
+        try (MetricsTimer timer =MetricsHelper.recordTime("getConceptIdForTypeTerm")) {
+            Map<String, String> params = new HashMap<>();
+            params.put("type", type);
+            params.put("term", term);
 
-        Response response = get(base + "/Term/Core", params);
+            Response response = get(base + "/Term/Core", params);
 
-        if (response.getStatus() == 200)
-            return response.readEntity(Integer.class);
-        else
-            throw new IOException(response.readEntity(String.class));
-
+            if (response.getStatus() == 200)
+                return response.readEntity(Integer.class);
+            else
+                throw new IOException(response.readEntity(String.class));
+        }
     }
 
     private static Response get(String path, Map<String, String> params) throws IOException {
-        if (config == null)
-            config = ObjectMapperPool.getInstance().readTree(ConfigManager.getConfiguration("api-internal", "information-model"));
+        if (kcConfig == null) {
+            kcConfig = ObjectMapperPool.getInstance().readTree(ConfigManager.getConfiguration("api-internal", "information-model"));
+            if (kcConfig == null)
+                throw new IllegalStateException("information-model/api-internal configuration not found!");
+        }
 
         if (kcClient == null)
             kcClient = new KeycloakClient(
-              config.get("auth-server-url").asText(),
-              config.get("realm").asText(),
-              config.get("username").asText(),
-              config.get("password").asText(),
+              kcConfig.get("auth-server-url").asText(),
+              kcConfig.get("realm").asText(),
+              kcConfig.get("username").asText(),
+              kcConfig.get("password").asText(),
               "information-model");
 
+        if (imUrl == null) {
+            imUrl = ConfigManager.getConfiguration("api");
+            if (imUrl == null)
+                throw new IllegalStateException("information-model/api configuration not found!");
+
+        }
+
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(config.get("im-url").asText()).path(path);
+        WebTarget target = client.target(imUrl).path(path);
 
         if (params != null && !params.isEmpty()) {
             for (Map.Entry<String, String> entry: params.entrySet()) {
