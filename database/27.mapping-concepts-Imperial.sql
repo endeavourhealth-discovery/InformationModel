2343280893b51e6bcec935beeca6d20ef7e98f38
suvithaSivaramanNHS <suvitha.sivaraman@nhs.net>
2021-02-26 13:05:11 +0000

    ADTS-15 FIN number changes

--- database/27.mapping-concepts-Imperial.sql
+++ database/27.mapping-concepts-Imperial.sql
@@ -12,8 +12,8 @@ VALUES
 (1, 'CM_Org_Imperial', @scm, 'CM_Org_Imperial', 'Imperial College', 'Imperial College Hospital, London'),
 (1, 'CM_Sys_Cerner', @scm, 'CM_Sys_Cerner', 'Cerner Millennium', 'Cerner Millennium system'),
 (1, 'ImperialCerner', @scm, 'ImperialCerner', 'Imperial Local Codes', 'Imperial Cerner local code scheme'),
-(1, 'CM_GPPractitionerId', @scm, 'CM_GPPractitionerId', 'GP Practitioner Id', 'GP Practitioner Id')
-;
+(1, 'CM_GPPractitionerId', @scm, 'CM_GPPractitionerId', 'GP Practitioner Id', 'GP Practitioner Id'),
+(1, 'CM_PatientFIN', @scm, 'CM_PatientFIN', 'Patient FIN', 'Patient FIN');
 
 -- Code scheme prefix entries
 INSERT IGNORE INTO concept_property_data
