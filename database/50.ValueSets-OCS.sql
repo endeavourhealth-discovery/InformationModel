ba666620bca18086f023754388ffb18c4ad46e55
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-05-11 10:15:17 +0100

    Oxford/Cambridge study value set

--- database/50.ValueSets-OCS.sql
+++ database/50.ValueSets-OCS.sql
@@ -2,23 +2,23 @@ DROP TABLE IF EXISTS value_set_member;
 DROP TABLE IF EXISTS value_set;
 
 CREATE TABLE value_set (
-                           dbid INT AUTO_INCREMENT,
-                           id VARCHAR(50) NOT NULL,
-                           name VARCHAR(150) NOT NULL,
+    dbid INT AUTO_INCREMENT,
+    id VARCHAR(50) NOT NULL,
+    name VARCHAR(150) NOT NULL,
 
-                           PRIMARY KEY value_set_pk (dbid),
-                           UNIQUE INDEX value_set_uq (id)
+    PRIMARY KEY value_set_pk (dbid),
+    UNIQUE INDEX value_set_uq (id)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE value_set_member (
-                                  dbid INT AUTO_INCREMENT,
-                                  value_set INT NOT NULL,
-                                  concept INT NOT NULL,
+    dbid INT AUTO_INCREMENT,
+    value_set INT NOT NULL,
+    concept INT NOT NULL,
 
-                                  PRIMARY KEY value_set_member_pk (dbid),
-                                  UNIQUE INDEX value_set_member_value_set_uq (value_set, concept),
+    PRIMARY KEY value_set_member_pk (dbid),
+    UNIQUE INDEX value_set_member_value_set_uq (value_set, concept),
 
-                                  FOREIGN KEY value_set_member_value_set_idx (value_set) REFERENCES concept (dbid)
+    FOREIGN KEY value_set_member_value_set_idx (value_set) REFERENCES concept (dbid)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 INSERT IGNORE INTO value_set (id, name) VALUES ('im:VSET_OCS', 'Oxford Covid Study value set');
@@ -48,6 +48,27 @@ WHERE c.scheme = @snomed
                  '373873005'
     );
 
+-- Add legacy maps
+SELECT @equiv := dbid FROM concept WHERE id = 'is_equivalent_to';
+
+INSERT IGNORE INTO value_set_member
+(value_set, concept)
+SELECT m.value_set, cpo.dbid
+FROM value_set_member m
+         JOIN concept_property_object cpo ON cpo.value = m.concept AND cpo.property = @equiv;
+
+-- Add in CTV3 using R2 maps where missing
+SELECT @read2 := dbid FROM concept WHERE id = 'READ2';
+SELECT @ctv3 := dbid FROM concept WHERE id = 'CTV3';
+
+INSERT IGNORE INTO value_set_member
+(value_set, concept)
+SELECT m2.value_set, r3.dbid
+FROM value_set_member m2
+         JOIN concept r2 ON r2.dbid = m2.concept AND r2.scheme = @read2
+         JOIN concept r3 ON r3.code = r2.code AND r3.scheme = @ctv3
+;
+
 -- Add ICD10
 SELECT @icd10 := dbid FROM concept WHERE id = 'ICD10';
 
@@ -66,39 +87,42 @@ SELECT @vset, c.dbid
 FROM concept c
 WHERE c.scheme = @opcs4;
 
+SELECT COUNT(*) FROM value_set_member;
 
+/*
 -- Test - Included SNOMED
 SELECT c.dbid
 FROM concept c
-         JOIN value_set_member m ON m.concept = c.dbid
-         JOIN value_set v ON v.dbid = m.value_set
+JOIN value_set_member m ON m.concept = c.dbid
+JOIN value_set v ON v.dbid = m.value_set
 WHERE v.id = 'im:VSET_OCS'
-  AND c.scheme = @snomed
-  AND c.code = '123980006';
+AND c.scheme = @snomed
+AND c.code = '123980006';
 
 -- Test - Excluded SNOMED
 SELECT c.dbid
 FROM concept c
-         JOIN value_set_member m ON m.concept = c.dbid
-         JOIN value_set v ON v.dbid = m.value_set
+JOIN value_set_member m ON m.concept = c.dbid
+JOIN value_set v ON v.dbid = m.value_set
 WHERE v.id = 'im:VSET_OCS'
-  AND c.scheme = @snomed
-  AND c.code = '123037004';
+AND c.scheme = @snomed
+AND c.code = '123037004';
 
--- Test - INCLUDED ICD10
+-- Test - INCLUDED icd10
 SELECT c.dbid
 FROM concept c
-         JOIN value_set_member m ON m.concept = c.dbid
-         JOIN value_set v ON v.dbid = m.value_set
+JOIN value_set_member m ON m.concept = c.dbid
+JOIN value_set v ON v.dbid = m.value_set
 WHERE v.id = 'im:VSET_OCS'
-  AND c.scheme = @icd10
-  AND c.code = 'A01.1';
+AND c.scheme = @icd10
+AND c.code = 'A01.1';
 
 -- Test - INCLUDED OPCS4
 SELECT c.dbid
 FROM concept c
-         JOIN value_set_member m ON m.concept = c.dbid
-         JOIN value_set v ON v.dbid = m.value_set
+JOIN concept s ON s.dbid = c.scheme AND s.id = 'OPCS4'
+JOIN value_set_member m ON m.concept = c.dbid
+JOIN value_set v ON v.dbid = m.value_set
 WHERE v.id = 'im:VSET_OCS'
-  AND c.scheme = @opcs4
-  AND c.code = 'A01.1';
+AND c.code = 'A01.1';
+*/
